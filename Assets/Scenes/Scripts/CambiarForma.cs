7d15937814d72b57732138d62db3e1a669eaccc1
losdecontenido <61255003+losdecontenido@users.noreply.github.com>
2020-03-09 17:56:14 -0600

    Añadido materiales fisicos y escena muestra
    Añadido:
    3 materiales fisicos
    3 materiales
    1 mesh
    7 texturas
    1 script
    Pendiente: Añadir el comportamiento de la plataforma y cambio de forma de personajes

---
+++ Assets/Scenes/Scripts/CambiarForma.cs
@@ -0,0 +1,107 @@
+﻿[UnityEngine.DisallowMultipleComponent]
+[UnityEngine.RequireComponent(typeof(UnityEngine.Rigidbody))]
+[UnityEngine.RequireComponent(typeof(UnityEngine.MeshRenderer))]
+public class CambiarForma : UnityEngine.MonoBehaviour
+{
+    //--|||--|||--
+    #region Propiedades Clase
+    [UnityEngine.SerializeField] private UnityEngine.PhysicMaterial [] materialCorrespondienteForma;
+    #endregion
+    //--|||--|||--
+    #region Propiedades Logica
+
+    #endregion
+    //--|||--|||--
+    #region Propiedades Constantes
+
+    #endregion
+    //--|||--|||--
+
+
+
+
+
+    //--|||--|||--
+    #region Metodos MonoBehaviour
+    private void Awake()
+    {
+    }
+    private void OnEnable()
+    {
+    }
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+    }
+    private void FixedUpdate()
+    {
+    }
+    private void LateUpdate()
+    {
+    }
+    private void OnDisable()
+    {
+    }
+    #endregion
+
+
+
+
+
+    //--|||--|||--
+    #region Metodos Clase
+    private void Metodo()
+    {
+    }
+    #endregion
+}
+/*
+[UnityEngine.Header("Propiedades")]
+public UnityEngine.Events.UnityEvent habilidadActivada;
+public EventoHabilidad eventoHabilidadLlamada;
+[System.Serializable] public class EventoHabilidad : UnityEngine.Events.UnityEvent<TipoHabilidadSeleccionado.Habilidad, short> { }
+public System.Collections.Generic.List<ListaAniadadaTransform> lista = new System.Collections.Generic.List<ListaAniadadaTransform>();
+
+[UnityEngine.SerializeField, UnityEngine.Tooltip("Descripcion de algo"), UnityEngine.Range(0, 9)] private short numero;
+[UnityEngine.Space]
+[UnityEngine.TextArea] public string texto = "El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja.";
+private string name = "Suresh Dasari";
+public string Name
+{
+    get
+    {
+      name.ToUpper();
+    }
+    set
+    {
+        if (value == "Suresh")
+            name = value;
+    }
+}
+//--|||--|||--
+[UnityEditor.MenuItem("Tools/Decir/Hola")]
+public static void Hola()
+{
+    Debug.Log("Hola mundo");
+}
+public struct NombreStructura
+{
+    int a = 0;
+    int z = 0;
+};
+public enum enumerador
+{
+    norte, sur, este, oeste
+};
+private System.Collections.IEnumerator Corrutina()
+{
+    yield return new UnityEngine.WaitForEndOfFrame();
+}
+[System.Serializable]
+public class ListaAniadadaTransform
+{
+    public System.Collections.Generic.List<UnityEngine.Transform> listaAnidada;
+}
+*/
