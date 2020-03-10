0f311da87793a8c8377a1fea6f26a39eb93f8723
Fernando Espino <fer9897@gmail.com>
2020-03-09 18:50:08 -0600

    Movimiento

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Joystick movementJoystick;
+
+    [SerializeField]
+    float playerSpeed;
+
+    Rigidbody rigidBody;
+
+    Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement.x = gameObject.transform.position.x + movementJoystick.Horizontal;
+        movement.z = gameObject.transform.position.z + movementJoystick.Vertical;
+        rigidBody.AddForce(movement * playerSpeed);
+    }
+}
